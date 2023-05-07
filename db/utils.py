aa075a3f541f73b52b5df69ec9395093c0749fcc
Nyagami <hoangquan05112002@gmail.com>
2023-05-07 23:05:30 +0700

    feat: login

--- db/utils.py
+++ db/utils.py
@@ -9,3 +9,12 @@ def connect() -> Connection:
                           f"Database=QLDSV;"
                           "Trusted_Connection=yes;")
     return conn
+
+def login(email, password):
+    conn = connect()
+    cursor = conn.cursor()
+    query = "SELECT ten, quyen FROM TaiKhoan WHERE email = ? AND CONVERT(nvarchar, mat_khau) = ?"
+
+    cursor.execute(query, email, password)
+    res = cursor.fetchone()
+    return res
