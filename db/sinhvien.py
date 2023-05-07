dae8d2a33d992659d838a80c38efa59d4691b26a
Nyagami <hoangquan05112002@gmail.com>
2023-05-07 11:57:56 +0700

    sinhvien query

--- db/sinhvien.py
+++ db/sinhvien.py
@@ -1,13 +1,31 @@
 from data.sinhvien import SinhVien
 from .utils import connect
+from pyodbc import DatabaseError
 
 def findAll() -> list[SinhVien]:
     query = """
-                SELECT ma, ten, gioi_tinh, ngay_sinh, gioi_tinh, lop, nganh
+                SELECT sv.ma, ten, ngay_sinh, gioi_tinh, lop, nganh
                 FROM 
-                    SinhVien JOIN Hoso ON SinhVien.ma_ho_so
+                    SinhVien as sv 
+                    JOIN Hoso as hs ON sv.ma_ho_so = hs.ma
             """
     conn = connect()
     cursor = conn.execute(query)
+    
+    res = []
     for row in cursor:
-        print(row)
+        res.append(SinhVien(row[0], row[1], row[2], row[3], row[4], row[5]))
+    cursor.close()
+    return res
+
+def update(sinhvien: SinhVien):
+    query = """
+                UPDATE Sinhvien SET lop = ?, WHERE ma = ?
+            """
+    conn = connect()
+    try:
+        conn.execute(query)
+    except DatabaseError as e:
+        print(e)
+        return None
+    return sinhvien
